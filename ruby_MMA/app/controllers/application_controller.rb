044d4ecd8e899c7aa3cad35baad5112fa2cb7904
Nicholas Zwiren <zwnick68@gmail.com>
2022-05-10 10:52:06 -0400

    made landing page have all data in json

--- ruby_MMA/app/controllers/application_controller.rb
+++ ruby_MMA/app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < Sinatra::Base
     set :default_content_type, "application/json"
     get '/' do
+        
      fighters = Fighter.all
-     fighters.to_json
+     managers = Manager.all
+     promotions = Promotion.all
+     
+     data = fighters + managers + promotions
+     data.to_json
     end
 end
